e5a958a48b2957d20173bd9cb4b5183817dbd9ce
sander90 <sander.shan@yahoo.com>
2016-07-27 19:34:43 +0800

    add com

---
+++ sanderPodTest/sanderPodTest.h
@@ -0,0 +1,13 @@
+//
+//  sanderPodTest.h
+//  sanderPodTest
+//
+//  Created by shansander on 16/7/27.
+//  Copyright © 2016年 shansander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sanderPodTest : NSObject
+- (void)printLogData;
+@end
---
+++ sanderPodTest/sanderPodTest.m
@@ -0,0 +1,20 @@
+//
+//  sanderPodTest.m
+//  sanderPodTest
+//
+//  Created by shansander on 16/7/27.
+//  Copyright © 2016年 shansander. All rights reserved.
+//
+
+#import "sanderPodTest.h"
+
+@implementation sanderPodTest
+
+- (void)printLogData
+{
+    printf("--------------");
+    printf("\nhello world\n");
+    printf("--------------\n");
+}
+
+@end
